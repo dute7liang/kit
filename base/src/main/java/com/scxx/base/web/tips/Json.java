0cc3939d039c98488c028b7bdb621f4cfa9fe56c
dute7liang <31098086+dute7liang@users.noreply.github.com>
2019-10-31 11:30:51 +0800

    更新一些细节文件

--- base/src/main/java/com/scxx/base/web/tips/Json.java
+++ base/src/main/java/com/scxx/base/web/tips/Json.java
@@ -11,6 +11,16 @@ public class Json extends Tip{
 
 	private Object t;
 
+	public Json(){}
+
+	public Json(String msg){
+		super.msg = msg;
+	}
+
+	public Json(boolean success,String msg){
+		super.success = success;
+		super.msg = msg;
+	}
 
 
 
